a9c73910ffdcb74fb657ca103ab12df3cbd63efb
Karston <kuciembakarston@gmail.com>
2023-12-16 18:52:06 -0600

    temporarily list top 100 exchanges + hide env

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,31 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [exchangeNames, setExchangeNames] = useState([]);
+
+  const getExchanges = async () => {
+    try {
+      const res = await axios.get(`https://rest.coinapi.io/v1/exchanges?apikey=${process.env.REACT_APP_API_KEY}`);
+      const names = res.data.map((item: any) => item.name);
+      setExchangeNames(names);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getExchanges();
+  }, []);
+
   return (
     <div className="App">
-      hello
+      <div>
+        <h2>Exchange Names:</h2>
+        {exchangeNames.map((name, index) => (
+          (index < 100) && (name !== '') && <div key={index}>{index + 1}. {name}</div>
+        ))}
+      </div>
     </div>
   );
 }
