a2c8a93c3db9274a3ee12eb4eead2ab57f64c6d6
Karston <kuciembakarston@gmail.com>
2023-12-17 22:57:36 -0600

    added background + changed menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,20 @@
-import React, { useEffect, useState } from 'react';
-import './appStyles.css';
-import axios from 'axios';
-import { Menu } from './components';
+import React, { useEffect, useState } from "react";
+import "./appStyles.css";
+import axios from "axios";
+import { Menu, Graph } from "./components";
+
 function App() {
   const [exchangeNames, setExchangeNames] = useState([]);
 
   const getExchanges = async () => {
     try {
-      const res = await axios.get(`https://rest.coinapi.io/v1/exchanges?apikey=${process.env.REACT_APP_API_KEY}`);
+      const res = await axios.get(
+        `https://rest.coinapi.io/v1/exchanges?apikey=${process.env.REACT_APP_API_KEY}`
+      );
       const names = res.data.map((item: any) => item.name);
-      console.log(exchangeNames)
+      console.log(exchangeNames);
       setExchangeNames(names);
-      console.log(exchangeNames)
+      console.log(exchangeNames);
     } catch (error) {
       console.log(error);
     }
@@ -24,6 +27,7 @@ function App() {
   return (
     <div className="App">
       <Menu />
+      <Graph page={1} />
     </div>
   );
 }
--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -1,8 +1,43 @@
-import React from 'react'
-import './menuStyles.css'
+import React, { useState } from "react";
+import "./menuStyles.css";
+import logo from "../../images/logo.png";
+import { Graph } from "../Graph";
 
 export function Menu() {
+  const [graphs, setGraphs] = useState<string[]>([]);
+
+  const handleGraphSelection = (index: number) => {
+    console.log(index);
+  };
   return (
-    <div className="header">Menu</div>
-  )
+    <div>
+      <div className="side-nav">
+        <a href="/" className="logo">
+          <img src={logo} alt="logo" className="logo-img" />
+        </a>
+        <ul className="nav-links">
+          <li>
+            <button>Home logo</button>
+          </li>
+          {graphs.map((graph, index) => (
+            <li key={index}>
+              <button onClick={() => handleGraphSelection(index)}>
+                Graph {index}
+              </button>
+            </li>
+          ))}
+          <li className="add-graph">
+            <button
+              className="add-graph-btn"
+              onClick={() =>
+                setGraphs((prevGraphs) => ["New Graph", ...prevGraphs])
+              }
+            >
+              +
+            </button>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 }
