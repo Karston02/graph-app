7c65360376c57fc061c8106a92022f12f3ceea4b
Karston <kuciembakarston@gmail.com>
2023-12-19 17:46:58 -0600

    changed modal to add new "graph" tab on menu

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,16 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import "./modalStyles.css";
+
 interface ModalProps {
   onClose: () => void;
+  onInputChange: (value: string) => void;
+  onAddGraph: () => void;
 }
-export function Modal({ onClose }: ModalProps) {
+
+export function Modal({ onClose, onInputChange, onAddGraph }: ModalProps) {
+  const [coinInput, setCoinInput] = useState("");
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { value } = event.target;
+    setCoinInput(value);
+    onInputChange(value); // Notify the parent component of the input change
+  };
+
+  const handleAddGraph = (event: React.FormEvent) => {
+    event.preventDefault();
+    onAddGraph(); // Notify the parent component to add a new graph
+  };
+
   return (
     <div className="modal-overlay">
       <div className="modal">
         <span className="close-button" onClick={onClose}>
           &times;
         </span>
-        <form action="">
+        <form onSubmit={handleAddGraph}>
           <div className="modal-content">
             <label>Enter the cryptocurrency</label>
             <input
@@ -19,8 +36,11 @@ export function Modal({ onClose }: ModalProps) {
               required
               placeholder="Ex: BTC"
               autoComplete="on"
+              value={coinInput}
+              onChange={handleInputChange}
             />
           </div>
+          <text className="modal-content">Please hit enter</text>
         </form>
       </div>
     </div>
