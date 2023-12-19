1a10f64bfa716b9b6f67e617fb9870a0793d955e
Karston <kuciembakarston@gmail.com>
2023-12-18 23:09:55 -0600

    changed graph flow

--- src/components/Graph/Graph.tsx
+++ src/components/Graph/Graph.tsx
@@ -1,23 +1,66 @@
 import React from "react";
-import ReactFlow, { MiniMap, Background, Controls } from "reactflow";
+import ReactFlow, { Background, Controls } from "reactflow";
 import "reactflow/dist/style.css";
 
-const initialNodes = [
+const nodeStyle = {
+  color: "#0041d0",
+  borderColor: "#0041d0",
+};
+
+export const nodes = [
   {
+    type: "input",
     id: "1",
-    position: { x: window.innerWidth / 2 - 100, y: window.innerHeight / 2 },
-    data: { label: "1" },
+    data: { label: "BTC" },
+    position: { x: 100, y: 0 },
+    style: nodeStyle,
   },
   {
     id: "2",
-    position: {
-      x: window.innerWidth / 2 - 100,
-      y: window.innerHeight / 2 + 100,
-    },
-    data: { label: "2" },
+    data: { label: "ETH" },
+    position: { x: 0, y: 100 },
+    style: nodeStyle,
+  },
+  {
+    id: "3",
+    data: { label: "LTC" },
+    position: { x: 200, y: 100 },
+    style: nodeStyle,
+  },
+  {
+    id: "4",
+    data: { label: "BONK" },
+    position: { x: 100, y: 200 },
+    style: nodeStyle,
   },
 ];
-const initialEdges = [{ id: "e1-2", source: "1", target: "2" }];
+
+export const edges = [
+  {
+    id: "1->2",
+    source: "1",
+    target: "2",
+    animated: true,
+  },
+  {
+    id: "1->3",
+    source: "1",
+    target: "3",
+    animated: true,
+  },
+  {
+    id: "2->4",
+    source: "2",
+    target: "4",
+    animated: true,
+  },
+  {
+    id: "3->4",
+    source: "3",
+    target: "4",
+    animated: true,
+  },
+];
 
 const removeMark = { hideAttribution: true };
 
@@ -29,13 +72,13 @@ export function Graph({ page }: GraphProps) {
   return (
     <div className="" style={{ height: "100vh", width: "100%" }}>
       <ReactFlow
-        minZoom={0.05}
-        maxZoom={2.0}
-        nodes={initialNodes}
-        edges={initialEdges}
+        defaultNodes={nodes}
+        defaultEdges={edges}
+        fitView
         proOptions={removeMark}
+        nodesDraggable
       >
-        <Background size={2} color="#7c7c80" />
+        <Background color="#282829" />
         <Controls />
       </ReactFlow>
     </div>
