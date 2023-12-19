be25a15eeeb385e73d3e66345da12aa7a70c0908
Karston <kuciembakarston@gmail.com>
2023-12-19 16:55:53 -0600

    backend for ML + HTTP req later

---
+++ src/server/routes/api.js
@@ -0,0 +1,2 @@
+// Define API routes that your React app can call to request predictions or other data 
+// from your Python scripts. Use Axios to make requests
---
+++ src/server/server.js
@@ -0,0 +1,2 @@
+// a simple Express server to handle requests from 
+// your React app and communicate with your Python machine learning code.
